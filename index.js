4a3b85620fc974f70908f996cf8ec746c3593e79
Madhav Anadkat <madhav.anadkat@thegatewaycorp.com>
2020-05-08 18:31:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import NfcAppIndexV2 from 'react-native-nfc-manager/example/AppIndexV2'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => NfcAppIndexV2);
